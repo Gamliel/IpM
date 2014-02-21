9dc7bed49fc5aecb1dc628fb42983bc4e22e4500
KPI <kpi@BlackBook.(none)>
2014-02-21 10:46:41 +0000

    Using downloaded version of jquery

--- IpM/public/javascripts/query-all-server-data.js
+++ IpM/public/javascripts/query-all-server-data.js
@@ -35,7 +35,7 @@
 				var element1 = document.createElement("span");
 				element1.id = "serverData_" + serverData['id'];
 				element1.className = "table-cell"; 
-				element1.textContent = serverData['hostName'] + '@@'
+				element1.textContent = serverData['hostName'] + '@'
 						+ serverData['domain'] + ':' + serverData['port']
 						+ ' <' + serverData['ipAddress'] + '>';
 				newCell.appendChild(element1);
